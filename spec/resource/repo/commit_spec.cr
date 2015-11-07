4a54facd32f42c108a3ff1a09e573f8f0450519d
Pine Mizune <pinemz@gmail.com>
2015-11-07 15:37:44 +0900

    Update spec

--- spec/resource/repo/commit_spec.cr
+++ spec/resource/repo/commit_spec.cr
@@ -100,6 +100,7 @@ module GitHub::Simple
 
       commit.should be_a(Response::SingleCommit)
       commit.url.should eq("https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e")
+      commit.sha.should eq("6dcb09b5b57875f334f61aebed695e2e4193db5e")
     end
   end
 end
